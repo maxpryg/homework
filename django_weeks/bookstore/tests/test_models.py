b39bf194bd725c427f6feee77ffb0f3a5b531684
Maksim Pryhunov <maxpryg@gmail.com>
2022-06-21 11:24:58 +0300

    Add models tests

--- django_weeks/bookstore/tests/test_models.py
+++ django_weeks/bookstore/tests/test_models.py
@@ -1,29 +1,57 @@
 from django.test import TestCase
 
 from bookstore.models import Author, Book
+from bookstore.data import books, authors
 
 
 class AuthorModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        Author.objects.create(first_name='Mark', last_name='Lutz', age=81)
+        """Populate DB with authors and books"""
+        Author.create_authors_from_dict(authors)
+        Book.create_books_from_dict(books, authors)
 
     def test_last_name_max_length(self):
-            author = Author.objects.get(id=1)
-            max_length = author._meta.get_field('last_name').max_length
-            self.assertEqual(max_length, 20)
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('last_name').max_length
+        self.assertEqual(max_length, 20)
 
     def test_first_name_max_length(self):
-            author = Author.objects.get(id=1)
-            max_length = author._meta.get_field('first_name').max_length
-            self.assertEqual(max_length, 20)
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEqual(max_length, 20)
 
-    def test_object_name_is_last_name_comma_first_name(self):
-            author = Author.objects.get(id=1)
-            expected_object_name = f'{author.last_name}, {author.first_name}'
-            self.assertEqual(str(author), expected_object_name)
+    def test_author_string_representation(self):
+        author = Author.objects.get(id=1)
+        expected_author_name = f'{author.last_name} {author.first_name}'
+        self.assertEqual(str(author), expected_author_name)
 
-    def test_get_absolute_url(self):
-            author = Author.objects.get(id=1)
-            # This will also fail if the urlconf is not defined.
-            self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
+    def test_get_all_author_books(self):
+        author = Author.objects.get(id=1)
+        expected_author_books_count = Book.objects.filter(
+            author_id__exact=author.id).count()
+        author_books_count = Author.get_all_author_books(author).count()
+        self.assertEqual(author_books_count, expected_author_books_count)
+
+
+class BookModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        """Populate DB with authors and books"""
+        Author.create_authors_from_dict(authors)
+        Book.create_books_from_dict(books, authors)
+
+    def test_title_max_length(self):
+        book = Book.objects.get(id=1)
+        max_length = book._meta.get_field('title').max_length
+        self.assertEqual(max_length, 100)
+
+    def test_description_max_length(self):
+        book = Book.objects.get(id=1)
+        max_length = book._meta.get_field('description').max_length
+        self.assertEqual(max_length, 1000)
+
+    def test_book_string_representation(self):
+        book = Book.objects.get(id=1)
+        expected_book_name = f'{book.title}, {book.author}'
+        self.assertEqual(str(book), expected_book_name)
