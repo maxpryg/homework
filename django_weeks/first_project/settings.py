857b130faa36d8741b10624ed94d0731622eada3
Maksim Pryhunov <maxpryg@gmail.com>
2022-06-27 11:04:12 +0300

    Add accounts app

--- django_weeks/first_project/settings.py
+++ django_weeks/first_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'firstapp',
     'bookstore',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/bookstore/"
+LOGOUT_REDIRECT_URL = "/bookstore/"
